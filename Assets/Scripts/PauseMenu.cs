25001b633d18fd374fdc27f228452311d837ab82
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-04-07 16:01:29 +0200

    add main menu and pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+    private const string MAIN_MENU_NAME = "StartMenu"; 
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(MAIN_MENU_NAME);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
